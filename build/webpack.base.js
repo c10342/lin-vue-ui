be214577b481cedf0ce3b2f5d060a32f0ee70a88
c10342 <1376023740@qq.com>
2021-02-22 17:04:13 +0800

    build(webpack.base.js): 配置typescript

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,36 +1,53 @@
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
-const ESLintPlugin = require('eslint-webpack-plugin');
-const StylelintPlugin = require('stylelint-webpack-plugin');
+const ESLintPlugin = require("eslint-webpack-plugin");
+const StylelintPlugin = require("stylelint-webpack-plugin");
 const path = require("path");
 const baseConfig = {
   stats: {
     modules: false,
     children: false,
     chunks: false,
-    chunkModules: false,
+    chunkModules: false
   },
   resolve: {
-    extensions: [".js", ".jsx", "md", ".vue", ".json"],
+    extensions: [".ts", ".tsx", ".js", ".jsx", "md", ".vue", ".json"],
     alias: {
       packages: path.join(__dirname, "../packages"),
       "lin-view-ui": path.join(__dirname, "../src/index.js"),
-      src: path.join(__dirname, "../src"),
-    },
+      src: path.join(__dirname, "../src")
+    }
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         use: ["babel-loader"],
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
       {
         test: /\.vue$/,
-        loader: "vue-loader",
+        loader: "vue-loader"
       },
-    ],
+      {
+        // 添加tsloader
+        test: /\.tsx?$/,
+        use: [
+          "babel-loader",
+          {
+            loader: "ts-loader",
+            options: {
+              appendTsSuffixTo: [/\.vue$/]
+            }
+          }
+        ],
+        exclude: /node_modules/
+        // options: {
+        //   appendTsSuffixTo: [/\.vue$/],
+        // }
+      }
+    ]
   },
-  plugins: [new VueLoaderPlugin(),new ESLintPlugin(),new StylelintPlugin()],
+  plugins: [new VueLoaderPlugin(), new ESLintPlugin(), new StylelintPlugin()]
 };
 
 module.exports = baseConfig;
