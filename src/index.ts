49725813b33723bc7dbe55b7bd552f6bec7a2251
c10342 <1376023740@qq.com>
2021-02-22 17:41:34 +0800

    refactor(index.ts): typescript重构入口主文件

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,29 @@ import 'src/fonts/iconfont.css';
 const testComps = require.context(
   '../packages',
   true,
-  /^\.(\/\w+)\/index\.js$/
+  /^\.(\/\w+)\/index\.(js|ts)$/
 );
 
-const reg = /^\.\/(\w+)\/index\.js$/;
+const reg = /^\.\/(\w+)\/index\.(js|ts)$/;
 
 const componentObjs = {};
-testComps.keys().forEach((key) => {
+testComps.keys().forEach((key:string) => {
   const componentEntity = testComps(key).default;
-  const result = reg.exec(key)[1];
-  componentObjs[result] = componentEntity;
+  const result = reg.exec(key);
+  if (result) {
+    componentObjs[result[1]] = componentEntity;
+  }
+  // const result = reg.exec(key)![1];
+  // componentObjs[result] = componentEntity;
 });
 
-const install = (Vue, opts = {}) => {
+interface Opts{
+  locale?:string,
+  // 映射 ==》 {[key:string]:unknown}
+  i18n?:Record<string, unknown>
+}
+
+const install = (Vue, opts:Opts = {}) => {
   locale.use(opts.locale);
   locale.i18n(opts.i18n);
   Object.keys(componentObjs).forEach((key) => {
