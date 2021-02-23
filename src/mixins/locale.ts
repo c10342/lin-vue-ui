02070f9f3bb6123ffba415a0f42f06897787b502
c10342 <1376023740@qq.com>
2021-02-23 16:01:36 +0800

    style(locale): 修改t方法引入的文件路径

--- src/mixins/locale.ts
+++ src/mixins/locale.ts
@@ -1,10 +1,10 @@
-import { t } from 'src/locale/index.js';
+import { t } from 'src/locale/index';
 
 import { Vue, Component } from 'vue-property-decorator';
 
 @Component
 export default class LocaleMixin extends Vue {
-  t (...args) {
+  t (...args:any) {
     return t.apply(this, args);
   }
 }
