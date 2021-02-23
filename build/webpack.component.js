e1f3ffa11aa05f3ad06d0ef7a6e7a023091652dc
c10342 <1376023740@qq.com>
2021-02-23 17:41:30 +0800

    build(webpack.component.js): 获取externals列表方法放到util.js文件下

--- build/webpack.component.js
+++ build/webpack.component.js
@@ -8,25 +8,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const baseConfig = require("./webpack.build.base");
 
-const packageJson = require("../package.json");
-
-const dependencies = {};
-
-for (const key in packageJson.dependencies) {
-  dependencies[key] = {
-    root: key,
-    commonjs: key,
-    commonjs2: key,
-    amd: key,
-  };
-}
-
-const resolve = (dir) => path.resolve(__dirname, "../src", dir);
-
-const utilsList = fs.readdirSync(resolve("utils"));
-const mixinsList = fs.readdirSync(resolve("mixins"));
-const jsList = fs.readdirSync(resolve("js"));
-const imageList = fs.readdirSync(resolve("images"));
+const util = require('./util')
 
 const getComponentEntries = (pathStr) => {
   let files = fs.readdirSync(path.join(__dirname, "../", pathStr));
@@ -37,52 +19,6 @@ const getComponentEntries = (pathStr) => {
     return ret;
   }, {});
   return componentEntries;
-};
-
-const getExternalsList = () => {
-  const externals = {
-    vue: {
-      root: "Vue",
-      commonjs: "vue",
-      commonjs2: "vue",
-      amd: "vue",
-    },
-    ...dependencies,
-    "src/locale/index.ts": "lin-view-ui/lib/assets/locale/index.js",
-    "src/locale/lang/zh-CN.ts": "lin-view-ui/lib/assets/locale/lang/zh-CN.js",
-    "src/locale/lang/en-US.ts": "lin-view-ui/lib/assets/locale/lang/en-US.js",
-    "src/locale/format.ts": "lin-view-ui/lib/assets/locale/format.js",
-    "src/fonts/iconfont.css": "lin-view-ui/src/fonts/iconfont.css",
-    "flv.js/dist/flv.js": "flv.js/dist/flv.js",
-  };
-
-  utilsList.forEach(function(file) {
-    file = path.basename(file);
-    externals[`src/utils/${file}`] = `lin-view-ui/lib/assets/utils/${file}`;
-  });
-  mixinsList.forEach(function(file) {
-    file = path.basename(file);
-    externals[`src/mixins/${file}`] = `lin-view-ui/lib/assets/mixins/${file}`;
-  });
-  jsList.forEach(function(file) {
-    file = path.basename(file);
-    externals[`src/js/${file}`] = `lin-view-ui/lib/assets/js/${file}`;
-  });
-  imageList.forEach(function(file) {
-    file = path.basename(file);
-    externals[`src/images/${file}`] = `lin-view-ui/src/images/${file}`;
-  });
-
-  const externalsObj = {}
-  Object.keys(externals).forEach(key=>{
-    let value = externals[key]
-    if(typeof key ==='string' && typeof value==='string'){
-      key = key.replace(/\.ts$/,'')
-      value = value.replace(/\.ts$/,'.js')
-    }
-    externalsObj[key] = value
-  })
-  return externalsObj;
 };
 
 const output = path.resolve(__dirname, "../lib");
@@ -98,7 +34,7 @@ const componentConfig = {
     library: "[name]",
   },
   externals: {
-    ...getExternalsList(),
+    ...util.getExternalsList(),
   },
   plugins: [
     new MiniCssExtractPlugin({
